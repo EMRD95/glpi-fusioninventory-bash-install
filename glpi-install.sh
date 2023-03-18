d880f5ff1f15942322ac815103de4c616a29b930
EMRD95 <114953576+EMRD95@users.noreply.github.com>
2023-03-18 11:18:00 +0000

    Update glpi-install.sh

--- glpi-install.sh
+++ glpi-install.sh
@@ -201,13 +201,13 @@ server {
         listen 80 default_server;
         listen [::]:80 default_server;
 
-                root /var/www/html/glpi;
+        root /var/www/html/glpi;
         index index.php index.html index.htm;
 
         server_name _;
 
         location / {
-                try_files $uri $uri/ =404;
+                try_files \$uri \$uri/ =404;
         }
 
         location ~ \.php$ {
